a228dfc51b43c8711c482894fc050a9eefcf1aa7
송민진 <m_6595@naver.com>
2023-09-05 17:17:57 +0900

    feat: 테스트 완료

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.post("/")
+@app.get("/")
 async def abc():
     response = ''
+    print('abc')
     return {response}
 
 
@@ -12,4 +13,4 @@ async def abc():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
