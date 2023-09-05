763edf1fb3f24f833d17a72ca7a023fd79ff9ff2
송민진 <m_6595@naver.com>
2023-09-05 18:58:03 +0900

    feat: OCR로 문자 추출하기 완료

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import uuid
+import time
+import config
 
 app = FastAPI()
 
+class Item(BaseModel):
+    imgUrl: str
+
+# 테스트용 메서드
 @app.get("/")
 async def test():
     response = 'GET 메서드 실행 완료'
     return {response}
+
+# 클로바 OCR
+@app.post("/ocr")
+async def send_request(item: Item):
+    url = config.OCR_URL
+    headers = {
+        'X-OCR-SECRET': config.X_OCR_SECRET,
+        'Content-Type': 'application/json',
+    }
+    data = {
+        "version": "V1",
+        "requestId": str(uuid.uuid4()),
+        "timestamp": int(round(time.time() * 1000)),
+        "lang": "ko",
+        "images": [
+            {
+                "format": "jpg",
+                "name": "demo",
+                "url": item.imgUrl
+            }
+        ],
+    }
+    ocrResponse = requests.post(url, headers=headers, json=data)
+
+    result = ' '.join([field['inferText'] for image in ocrResponse.json()['images'] for field in image['fields']])
+
+    response = {"result": result}
+    return response
 
 
 # FastAPI 앱 실행
