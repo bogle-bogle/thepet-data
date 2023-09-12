82a2efe222ece301a7ac9eda461f2f51085992a4
송민진 <m_6595@naver.com>
2023-09-12 22:14:11 +0900

    feat: 소요시간 로그 찍기 및 텍스트로 유사도 확인하는 메서드 추가
    #11

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import APIRouter
 from pydantic import BaseModel
 
@@ -39,8 +41,19 @@ async def calculate_img_to_similarity(item: ImgItem):
     finalResult = get_recommendations_with_new_data(gptResult)
     return finalResult
 
-@router.post("/convert-to-similarity")
+@router.post("/img-to-similarity")
 async def calculate_img_to_similarity(item: ImgItem):
+    start = time.time()
     ocr_result = extract_full_content_with_ocr(item)
     final_result = get_most_similar_top_ten(ocr_result.replace(", ", ",").split(","))
+    end = time.time()
+    print("소요 시간 : " + str(end - start))
+    return final_result
+
+@router.post("/txt-to-similarity")
+async def calculate_img_to_similarity(item: TxtItem):
+    start = time.time()
+    final_result = get_most_similar_top_ten(item.content.replace(", ", ",").split(","))
+    end = time.time()
+    print("소요 시간 : " + str(end - start))
     return final_result
