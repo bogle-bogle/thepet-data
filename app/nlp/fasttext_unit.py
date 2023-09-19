e8763ad38caa0d9aab90d63593174e67b6f68c4b
youmin <super_vet@naver.com>
2023-09-19 18:03:48 +0900

    feat(fasttext) : 모델 미리 로드 및 추가 병렬처리

--- app/nlp/fasttext_unit.py
+++ app/nlp/fasttext_unit.py
@@ -1,60 +1,66 @@
 import math
 import time
-
 import numpy as np
 import pandas as pd
 import pickle
-
 from gensim.models import FastText
-from joblib import Parallel, delayed
 from sklearn.metrics.pairwise import cosine_similarity
-
+from joblib import Parallel, delayed
 from config import *
 
+# 미리 모델 로드
+fasttext_loaded_model = FastText.load(FASTTEXT_INGREDIENT_MODEL_PATH)
+with open(INGREDIENT_CLUSTER_PATH, "rb") as file:
+    ingredient_clusters = pickle.load(file)
 
 # 성분이 앞에 있을 수록 가중치 계산
-def calculate_weighted_vector(ingredients, loaded_model, max_weight=1.0, decay_factor=0.95):
+def calculate_weighted_vector(ingredients, max_weight=1.0, decay_factor=0.95):
     total_weight = 0
-    weighted_vector = np.zeros(loaded_model.vector_size)
+    weighted_vector = np.zeros(fasttext_loaded_model.vector_size)
     for idx, ingredient in enumerate(ingredients):
-        if ingredient in loaded_model.wv:
+        if ingredient in fasttext_loaded_model.wv:
             weight = max_weight * (decay_factor ** idx)
             total_weight += weight
-            weighted_vector += weight * loaded_model.wv[ingredient]
+            weighted_vector += weight * fasttext_loaded_model.wv[ingredient]
     return weighted_vector / total_weight
 
+# 병렬 처리 함수
+def calculate_similarity_batch(rows, user_vector, weight):
+    similarities = []
+    for _, row in rows.iterrows():
+        pet_food_ingredients = row['INGREDIENTS'].split(', ')
+        pet_food_vector = calculate_weighted_vector(pet_food_ingredients).reshape(1, -1)
 
-# 느려서 병렬 처리하려고 함수 따로 만듦
-def calculate_similarity(row, user_vector, ingredient_clusters, loaded_model, weight):
-    pet_food_ingredients = row['INGREDIENTS'].split(', ')
-    pet_food_vector = calculate_weighted_vector(pet_food_ingredients, loaded_model).reshape(1, -1)
+        # 유사도 계산
+        similarity = cosine_similarity(user_vector, pet_food_vector)[0][0]
 
-    # 유사도 계산
-    similarity = cosine_similarity(user_vector, pet_food_vector)[0][0]
+        # 사료 제목에서 ingredient_clusters의 단어가 포함된 경우 가중치를 부여
+        for ingredient in ingredient_clusters:
+            if any(clustered_ingredient in row['NAME'] for clustered_ingredient in ingredient_clusters[ingredient]):
+                similarity *= weight
+                # break  # 하나라도 있으면 break
 
-    # 사료 제목에서 ingredient_counts의 단어가 포함된 경우 가중치를 부여
-    for ingredient in ingredient_clusters:
-        if any(clustered_ingredient in row['NAME'] for clustered_ingredient in ingredient_clusters[ingredient]):
-            similarity *= weight
-            # break  # 하나라도 있으면 break
-
-    return (row['NAME'], similarity)
+        similarities.append((row['NAME'], similarity))
+    return similarities
 
 # 사용자가 입력한 성분 리스트를 바탕으로 유사한 사료를 10개 추천!
 # 제목에 가중치 1.2배 줌
 def get_most_similar_top_nine(user_ingredients, weight=1):
     start = time.time()
-
-    fasttext_loaded_model = FastText.load(FASTTEXT_INGREDIENT_MODEL_PATH)
-    user_vector = calculate_weighted_vector(user_ingredients, fasttext_loaded_model).reshape(1, -1)
+    user_vector = calculate_weighted_vector(user_ingredients).reshape(1, -1)
     product_data = pd.read_csv(PRODUCT_RAW_DATA_PATH)
-    with open(INGREDIENT_CLUSTER_PATH, "rb") as file:
-        ingredient_clusters = pickle.load(file)
+
+    # 데이터를 작은 배치로 분할
+    batch_size = 100  # 적절한 배치 크기를 선택하세요.
+    num_batches = len(product_data) // batch_size + 1
+    batches = np.array_split(product_data, num_batches)
 
     # 병렬 처리
     similarities = Parallel(n_jobs=-1)(
-        delayed(calculate_similarity)(row, user_vector, ingredient_clusters, fasttext_loaded_model, weight) for _, row in
-        product_data.iterrows())
+        delayed(calculate_similarity_batch)(batch, user_vector, weight) for batch in batches)
+
+    # 병렬 처리 결과를 평탄화
+    similarities = [item for sublist in similarities for item in sublist]
 
     # 유사도가 높은 상위 10개의 사료를 반환
     result = []
